ea9a3d438b8e0bde21e612162357d0cd39c9a6c0
wanggc <15051183877@163.com>
2019-04-12 13:20:46 +0800

    Component State

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,63 @@
 import styles from './index.css';
+import {Button,Tag} from 'antd';
+import React,{Component} from 'react';
+import {connect} from 'dva';
+const typeMap = ["dashed","normal","danger","primary"];
 
+// class MyButton extends Component{
 
-export default function() {
-  return (
+//   render(){
+//     return (
+//         <Button type={this.props.type} onClick={this.props.typeChange}>
+//           Click Me
+//         </Button>
+//     )
+//   }
+// }
+
+// const mapStateToProps = (state) =>{
+//   return {
+//     mystate: state.typecount
+//   }
+// }
+
+// const mapDispatchToProps = (dispatch) =>{
+//   return {
+//     click: ()=>dispatch({type:"typecount/click",payload:{photo:"clothes"}}),
+//     get: ()=>dispatch({type:"typecount/get"})
+//   }
+// }
+
+// @connect(mapStateToProps,mapDispatchToProps)
+class MyPage extends Component{
+  state = {
+    type: "dashed",
+    count: 0,
+  }
+  render(){
+    // console.log(this.props.mystate);
+    return(
     <div className={styles.normal}>
-      <ul className={styles.list}>
-        <h1 style={{color:"blue"}}>Add some code,...</h1>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
-        <li>
-          <a href="https://umijs.org/guide/getting-started.html">
-            Getting Started
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+    <ul className={styles.list}>
+      <Button type={this.state.type} onClick={()=>{
+        let count = this.state.count+1;
+        this.setState({
+          type: typeMap[count%4],
+          count: count
+        })
+      }}>Click Me</Button>
+      <Tag color="lime">{this.state.count}</Tag>
+      {/* <MyButton  type={this.props.mystate.type} typeChange={this.props.get}/> */}
+      {/* <Tag color="lime">{this.props.mystate.count}</Tag> */}
+    </ul>
+    </div>)
+  }
 }
+
+export default MyPage;
+
+
+// function dispatch(action){
+//   bangdingdaodvamodel;
+//   return action;
+// }
